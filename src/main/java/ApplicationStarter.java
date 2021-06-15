abf3f5e74c82b2723277c6c82ea3275408b0a5b2
jesserVolleyball <j.l.e@gmx.de>
2021-06-15 16:38:50 +0200

    add an central List with clients and add it as parameter of ServerDistributor constructor

--- src/main/java/ApplicationStarter.java
+++ src/main/java/ApplicationStarter.java
@@ -1,5 +1,6 @@
 import server.DistributorRunner;
 import server.ServerDistributor;
+import server.ServerThread;
 
 import javax.net.ssl.SSLServerSocketFactory;
 import java.io.DataInputStream;
@@ -7,15 +8,20 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class ApplicationStarter {
 
     public static void main(String[] args) throws IOException {
+        //every client is listened in an individual thread
+        List<ServerThread> clients= Collections.synchronizedList(new ArrayList<>());;
         int[] ports = {13370, 13371};
         //one Thread per Port
         for (int port : ports
         ) {
-            Thread thread = new Thread(new DistributorRunner(port));
+            Thread thread = new Thread(new DistributorRunner(port,clients));
             thread.start();
         }
         System.out.println("server starts");
--- src/main/java/server/DistributorRunner.java
+++ src/main/java/server/DistributorRunner.java
@@ -1,10 +1,12 @@
 package server;
 
+import java.util.List;
+
 public class DistributorRunner implements Runnable{
 
     ServerDistributor distributor;
-    public DistributorRunner(int port){
-        this.distributor = new ServerDistributor(port);
+    public DistributorRunner(int port, List<ServerThread> clients){
+        this.distributor = new ServerDistributor(port, clients);
     }
     @Override
     public void run() {
